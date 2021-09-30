5aace0a0139954b6e934144c745bde7d6ee06839
ValeryRul <pratatupi@gmail.com>
2021-09-30 15:26:49 +0300

    change 86.19.js(2)

--- 86.19.js
+++ 86.19.js
@@ -2,12 +2,14 @@
 за исключением элементов из 2-ого массива*/
 
 function except(array, excluded){
-    for(let key in array){
-        if (Object.values(excluded).includes(array[key])){
-            array.splice(key,1);
+    let indexInArray;
+    for(let key in excluded){
+        indexInArray = array.indexOf(excluded[key]);
+        if(indexInArray !== -1){
+        array.splice(indexInArray, 1)
         }
     }
     return array;
 }
 
-console.log(except([1,2,3,4,5,6,7,0], [1 ," ",5,7]));
+console.log(except([1,2,3,4,5,6,7, -2, 120], [1,2,3,4,5,6,7, -1, 120 ]));
